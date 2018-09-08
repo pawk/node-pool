599613b535b200a9650e6cfc4fe3f21373211fea
Paweł Kornijów <pawel.kornijow@intive.com>
2018-09-08 17:46:48 +0200

    restart functionality

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { fork, setupMaster } = require('cluster');
+const cluster = require('cluster');
 const { cpus } = require('os');
 
-setupMaster({
+cluster.setupMaster({
   exec: 'src/worker.js'
 });
+cluster.on('exit', restart);
 
 console.log('forking for %d cores', cpus().length);
-const children = cpus().map(fork);
+const children = cpus()
+  .map(cluster.fork);
+
+function restart(worker, code, signal) {
+  console.log('worker %d died (%s). restarting...',
+              worker.process.pid, signal || code);
+  cluster.fork();
+}
--- src/worker.js
+++ src/worker.js
@@ -1,3 +1,10 @@
 const { pid } = process;
 
 console.log('hello from %d', pid);
+
+const rand = Math.random();
+
+if (rand < 0.5) {
+  console.log(rand)
+  process.exit(2)
+}
