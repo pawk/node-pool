085d1839f84a6eda7d0578262f6a47f55251eba3
Paweł Kornijów <pawel.kornijow@intive.com>
2018-09-08 21:09:46 +0200

    commands refactor

--- src/terminal.js
+++ src/terminal.js
@@ -1,5 +1,30 @@
 const readline = require('readline');
 
+const commands = {
+  ls: function(line, cluster) {
+    console.log('list of running workers: %s\n', Reflect.ownKeys(cluster.workers).join(', '));
+  },
+  kill: function(line, cluster) {
+    const [ , num ] = line.split(' ');
+    if (Number.isNaN(parseInt(num))) {
+      console.log('usage: kill [num]\n');
+      return;
+    }
+    if (num in cluster.workers) {
+      console.log('killing %d\n', num);
+      cluster.workers[num].kill();
+    } else {
+      console.log('there is no worker %d\n', num);
+    }
+  },
+  exit: function(line, cluster) {
+    readline.moveCursor(process.stdout, 0, -1);
+    console.log('\nbye!\n');
+    process.exit(0);
+  }
+};
+
+
 module.exports = function terminal(cluster) {
   const terminal = readline.createInterface({
     input: process.stdin,
@@ -10,34 +35,18 @@ module.exports = function terminal(cluster) {
   terminal.on('line', handleLine)
 
   function handleLine(line) {
-    execute(line);
+    parse(line);
     readline.moveCursor(process.stdout, 0, -1);
     terminal.prompt();
   }
 
-  function execute(line) {
-    if (line === 'exit') {
-      readline.moveCursor(process.stdout, 0, -1);
-      console.log('\nbye!\n');
-      process.exit(0);
-    } else if (line === 'ls') {
-      readline.moveCursor(process.stdout, 0, -1);
-      Reflect.ownKeys(cluster.workers)
-        .map(num => console.log(num));
-        console.log('\n');
-    } else if (/^kill/.test(line)) {
-      readline.moveCursor(process.stdout, 0, -1);
-      const [ , num ] = line.split(' ');
-      if (Number.isNaN(parseInt(num))) {
-        console.log('usage: kill [num]\n');
-        return;
-      }
-      if (num in cluster.workers) {
-        console.log('killing %d\n', num);
-        cluster.workers[num].kill();
-      } else {
-        console.log('there is no worker %d\n', num);
-      }
+  function parse(line) {
+    readline.moveCursor(process.stdout, 0, -1);
+    const command = line.split(' ')[0];
+    if (command in commands) {
+      commands[command](line, cluster);
+    } else {
+      console.log('available commands are: %s\n', Reflect.ownKeys(commands).join(', '))
     }
   }
 }
