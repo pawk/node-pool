f8910ec345215a6f8a3af3a033acf04f14a64507
Paweł Kornijów <pawel.kornijow@intive.com>
2018-09-08 17:50:23 +0200

    refactor

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const cluster = require('cluster');
 const { cpus } = require('os');
 
-cluster.setupMaster({
-  exec: 'src/worker.js'
-});
-cluster.on('exit', restart);
+run();
 
-console.log('forking for %d cores', cpus().length);
-const children = cpus()
-  .map(cluster.fork);
+function run() {
+  configure();
+  fork();
+}
+
+function configure() {
+  cluster.setupMaster({
+    exec: 'src/worker.js'
+  });
+  cluster.on('exit', restart);
+}
+
+function fork() {
+  console.log('forking for %d cores', cpus().length);
+  const children = cpus().map(cluster.fork);
+}
 
 function restart(worker, code, signal) {
-  console.log('worker %d died (%s). restarting...',
-              worker.process.pid, signal || code);
+  console.log(
+    'worker %d died (%s), restarting...', worker.process.pid, signal || code
+  );
   cluster.fork();
 }
--- src/worker.js
+++ src/worker.js
@@ -5,6 +5,6 @@ console.log('hello from %d', pid);
 const rand = Math.random();
 
 if (rand < 0.5) {
-  console.log(rand)
-  process.exit(2)
+  console.log('worker %d will now throw!', pid);
+  throw Error();
 }
